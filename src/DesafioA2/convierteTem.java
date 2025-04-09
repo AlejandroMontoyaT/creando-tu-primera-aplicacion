33da7b99a9e70275af750b9d2384a0e857d610d1
Alejandro Montoya <isc.alejandro14@gmail.com>
2025-04-09 12:39:08 -0600

    se sube rama del modulo 2

---
+++ src/DesafioA2/convierteTem.java
@@ -0,0 +1,16 @@
+package DesafioA2;
+
+public class convierteTem {
+
+    public static void main(String[] args) {
+       //convierta una temperatura en grados Celsius a Fahrenheit
+        // Paso 1: Definir la temperatura en Celsius
+        double celsius = 25.0;
+
+        // Paso 2: Realizar la conversión a Fahrenheit
+        double fahrenheit = (celsius * 1.8) + 32;
+
+        // Paso 3: Mostrar el resultado
+        System.out.println("La temperatura de Grados C° a F° es: " + fahrenheit);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Screen Match");
+        System.out.println("Pelicula: Matrix");
+
+       int fechaDeLanzamiento = 1999;
+       boolean incruidoEnElPlan = true;
+       double notaDeLaPelicula = 8.2;
+
+       double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println("Media: " + media);
+
+        String sinopsis = """
+                Un hacker llamado Neo investiga sobre una misteriosa realidad llamada 
+                Matrix y se une a un grupo de rebeldes que luchan contra las máquinas que dominan el mundo.
+                La película explora temas de realidad virtual, control mental y la lucha por la libertad.""" + fechaDeLanzamiento;
+        System.out.println("Sinopsis: " + sinopsis);
+
+        //casteo o conversion de tipos
+        int clasificacion = (int) (media / 2);
+        System.out.println("Clasificacion: " + clasificacion);
     }
 }
